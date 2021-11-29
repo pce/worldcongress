dc709c95a7262abb5c9ed2dae1ed264150785ab4
pce <patrick@madhat.de>
2021-11-30 00:25:34 +0100

    init

---
+++ src/client/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <p>
+          XERUS
+        </p>
+        <a
+          className="app-link"
+          href="/room/123"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Join
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
